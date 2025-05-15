2c368b92e4426e6e5db622a60ddef4bac9e8a5d6
Maria Eduarda <mariaeduardareistds.senai@gmail.com>
2025-05-15 08:26:32 -0300

    refactor: initial configs

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Games Retrô</h1>
+      <p> Recordes de jogos retro</p>
+    </main>
+  );
+}
